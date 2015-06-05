a98ad07e48334fd9e47985c8a00b403cdd67f78c
Ryan - David Reyes <ryan.david.reyes@gmail.com>
2015-06-05 01:48:03 -0400

    Fully working server with x_based client.
    Moved the settings into a data structure for tidyness.
    Added -x/--xtended_features flag for allowing the usage of x_based
    client.
    
    The server is now capable of transmitting and receiving the x_based data
    structure that contains the modifier, press/release, and the key.
    
    The server will apply the modifiers to the keys pressed based on the
    above data, according to the mapping. Mappings had to be done for
    special keys such as pgup, pgdown, home, and punctuation keys.

--- main.cpp
+++ main.cpp
@@ -6,12 +6,27 @@
 #include <iostream>
 #include <iomanip>
 #include <cstring>
+#include <inttypes.h>
 #include <windows.h>
 #include <tcpipwin32.h>
+
+struct settings_t {
+  int port;
+  int tcp_socket;
+  bool enable_xtended_feat;
+};
+
+struct x_key_received_t {
+  char modifier;
+  uint16_t key;
+  bool key_press;
+};
 
 const int DEFAULT_PORT = 25525;
 
 char ConvertKey(const char key);
+char XConvertKey(const uint16_t key);
+char XTranslateModifier(char modifier);
 void SendVirtualKeyTap(INPUT &input);
 void SendVirtualKeyHold(INPUT &input);
 void SendVirtualKeyUp(INPUT &input);
@@ -21,8 +36,13 @@ void SendUnicodeCharacter(char key, INPUT &input);
 int main(int argc, char ** argv) {
   INPUT remote_user_input;
   TCP tcp_connection;
-  int port = DEFAULT_PORT,
-      tcp_socket;
+
+  settings_t settings;
+  settings.port = DEFAULT_PORT;
+  settings.tcp_socket = 0;
+  settings.enable_xtended_feat = false;
+  x_key_received_t x_key_received;
+
   char key_received = 0x00;
 
   //generic keyboard event
@@ -33,76 +53,121 @@ int main(int argc, char ** argv) {
   //parse the arguments
   if (argc > 1) {
     for (int i = 0; i < argc; i ++) {
-      if (  (strcmp(argv[i], "-p")) ||
-          (strcmp(argv[i], "--port") ) ) {
+      if (  (strcmp(argv[i], "-p") ==0) ||
+          (strcmp(argv[i], "--port") ==0) ) {
         if (i+1 < argc)
-          port = atoi(argv[i+1]);
+          settings.port = atoi(argv[i+1]);
       } //end if
+
+      else if ((strcmp(argv[i], "-x") ==0) ||
+          (strcmp(argv[i], "--xtended_features") == 0)) {
+        settings.enable_xtended_feat = true;
+        std::cout << "Extended features will be used.\n";
+      }
     } //end for
   } //end if
-  else {
-    std::cout << "Default port " << DEFAULT_PORT << " will be used.\n";
-  }
+  std::cout << "Port " << settings.port << " will be used.\n";
 
   //listen and accept an incoming connection
-  if (tcp_connection.listenToPort(port)) {
-    tcp_socket = tcp_connection.acceptConnection(); std::cout << "Connection accepted!\n";
+  if (tcp_connection.listenToPort(settings.port)) {
+    settings.tcp_socket = tcp_connection.acceptConnection(); std::cout << "Connection accepted!\n";
   }
 
-  //cout the keys
-  while (key_received != 0x04) {
-    if (tcp_connection.receiveData(tcp_socket, (char *) &key_received,
-        sizeof(key_received))) {
+  //use the extended features of the program
+  if (settings.enable_xtended_feat) {
+    while (1) {
+      if (tcp_connection.receiveData(settings.tcp_socket,
+            (char *) &x_key_received, sizeof(x_key_received))) {
 
-      std::cout << std::hex << (int)key_received << std::endl;
+        //if key press
+        if (x_key_received.key_press) {
+          //handle the modifier
+          if (x_key_received.modifier != 0) {
+            remote_user_input.ki.wVk = XTranslateModifier(x_key_received.modifier);
+            SendVirtualKeyHold(remote_user_input);
+          }
+          std::cout << "modifier " << std::hex << (int) x_key_received.modifier << "\n";
 
-      if (key_received == 0x08) { //ctrl-backspace
-        SendVirtualKeyChord(VK_CONTROL, VK_BACK, remote_user_input);
-      }
-      else if (key_received == 0x06) { //ctrl f
-        SendVirtualKeyChord(VK_CONTROL, 'F', remote_user_input);
-      }
-      else if (key_received == 0x10){ //ctrl p
-        SendVirtualKeyChord(VK_CONTROL, 'P', remote_user_input);
-      }
-      else if (key_received == 0x0E){ //ctrl n
-        SendVirtualKeyChord(VK_CONTROL, 'N', remote_user_input);
-      }
-      else if (key_received == 0x17) { //ctrl w
-        SendVirtualKeyChord(VK_CONTROL, 'W', remote_user_input);
-      }
-      else if (key_received == 0x14) { //ctrl t
-        SendVirtualKeyChord(VK_CONTROL, 'T', remote_user_input);
-      }
-      else if (key_received == 0x0c) { //ctrl l
-        SendVirtualKeyChord(VK_CONTROL, 'L', remote_user_input);
-      }
-      else if (key_received == 0x18) { //ctrl x
-        SendVirtualKeyChord(VK_CONTROL, 'X', remote_user_input);
-      }
-      else if (key_received == 0x16) { //ctrl v
-        SendVirtualKeyChord(VK_CONTROL, 'V', remote_user_input);
-      }
-      else if (key_received == 0x15) { //ctrl u mapped to ctrl-z
-        SendVirtualKeyChord(VK_CONTROL, 'Z', remote_user_input);
-      }
-      else if (key_received == 0x19) { //ctrl y mapped to ctrl-c
-        SendVirtualKeyChord(VK_CONTROL, 'C', remote_user_input);
-      }
-      else if (isupper(key_received)) { //all uppercase characters
-        SendVirtualKeyChord(VK_SHIFT, ConvertKey(key_received),
-            remote_user_input); }
-      else if (isdigit(key_received) || ispunct(key_received)) { //punctuation
-        SendUnicodeCharacter(key_received, remote_user_input);
-      }
-      else {
-        remote_user_input.ki.wVk = ConvertKey(key_received);
-        SendVirtualKeyTap(remote_user_input);
-      } //end else
-    } //end if
-    else
-      break;
-  } //end while
+            remote_user_input.ki.wVk = XConvertKey(x_key_received.key);
+            std::cout << "converted key " << std::hex << (uint8_t) XConvertKey(x_key_received.key) << "\n";
+
+            SendVirtualKeyHold(remote_user_input);
+          std::cout << "press " << std::hex << x_key_received.key << "\n";
+        } //end if (x_key_received.key_press)
+
+        //else if key_release
+        else {
+          //handle the modifier
+          if (x_key_received.modifier != 0) {
+            remote_user_input.ki.wVk = XTranslateModifier(x_key_received.modifier);
+            SendVirtualKeyUp(remote_user_input);
+          }
+
+          //handle the key press
+          remote_user_input.ki.wVk = XConvertKey(x_key_received.key);
+          SendVirtualKeyUp(remote_user_input);
+          std::cout << "release\n";
+        } //end else
+      } //end if (tcp_connection...
+    } //end while
+  } //if (settings.enable_xtended_feat)
+
+  else { //do not use the extended features
+    while (key_received != 0x04) {
+      //cout the keys
+      if (tcp_connection.receiveData(settings.tcp_socket, (char *) &key_received,
+            sizeof(key_received))) {
+
+        std::cout << std::hex << (int)key_received << std::endl;
+
+        if (key_received == 0x08) { //ctrl-backspace
+          SendVirtualKeyChord(VK_CONTROL, VK_BACK, remote_user_input);
+        }
+        else if (key_received == 0x06) { //ctrl f
+          SendVirtualKeyChord(VK_CONTROL, 'F', remote_user_input);
+        }
+        else if (key_received == 0x10){ //ctrl p
+          SendVirtualKeyChord(VK_CONTROL, 'P', remote_user_input);
+        }
+        else if (key_received == 0x0E){ //ctrl n
+          SendVirtualKeyChord(VK_CONTROL, 'N', remote_user_input);
+        }
+        else if (key_received == 0x17) { //ctrl w
+          SendVirtualKeyChord(VK_CONTROL, 'W', remote_user_input);
+        }
+        else if (key_received == 0x14) { //ctrl t
+          SendVirtualKeyChord(VK_CONTROL, 'T', remote_user_input);
+        }
+        else if (key_received == 0x0c) { //ctrl l
+          SendVirtualKeyChord(VK_CONTROL, 'L', remote_user_input);
+        }
+        else if (key_received == 0x18) { //ctrl x
+          SendVirtualKeyChord(VK_CONTROL, 'X', remote_user_input);
+        }
+        else if (key_received == 0x16) { //ctrl v
+          SendVirtualKeyChord(VK_CONTROL, 'V', remote_user_input);
+        }
+        else if (key_received == 0x15) { //ctrl u mapped to ctrl-z
+          SendVirtualKeyChord(VK_CONTROL, 'Z', remote_user_input);
+        }
+        else if (key_received == 0x19) { //ctrl y mapped to ctrl-c
+          SendVirtualKeyChord(VK_CONTROL, 'C', remote_user_input);
+        }
+        else if (isupper(key_received)) { //all uppercase characters
+          SendVirtualKeyChord(VK_SHIFT, ConvertKey(key_received),
+              remote_user_input); }
+        else if (isdigit(key_received) || ispunct(key_received)) { //punctuation
+          SendUnicodeCharacter(key_received, remote_user_input);
+        }
+        else {
+          remote_user_input.ki.wVk = ConvertKey(key_received);
+          SendVirtualKeyTap(remote_user_input);
+        } //end else
+      } //end if
+      else
+        break;
+    } //end while
+  } //end else { //do not use extended features
 
   return 0;
 }
@@ -133,14 +198,14 @@ void SendVirtualKeyTap(INPUT &input) {
 }
 
 void SendVirtualKeyHold(INPUT &input) {
-      input.ki.dwFlags = 0;
-      input.ki.wScan = 0; //hardware scan code
-      SendInput(1, &input, sizeof(INPUT)); //send a key press
+  input.ki.dwFlags = 0;
+  input.ki.wScan = 0; //hardware scan code
+  SendInput(1, &input, sizeof(INPUT)); //send a key press
 }
 
 void SendVirtualKeyUp(INPUT &input) {
-      input.ki.dwFlags = KEYEVENTF_KEYUP;
-      SendInput(1, &input, sizeof(INPUT)); //send a key up signal
+  input.ki.dwFlags = KEYEVENTF_KEYUP;
+  SendInput(1, &input, sizeof(INPUT)); //send a key up signal
 }
 
 char ConvertKey(const char key) {
@@ -176,3 +241,148 @@ char ConvertKey(const char key) {
   }
   return converted_key;
 }
+
+
+char XConvertKey(const uint16_t key) {
+  char converted_key = '\0';
+  switch(key) {
+    case ' ':
+      converted_key = VK_SPACE;
+      break;
+    case 0xffff:
+      converted_key = VK_DELETE;
+      break;
+    case 0xff09:
+      converted_key = VK_TAB;
+      break;
+    case 0xff08:
+      converted_key = VK_BACK;
+      break;
+    case 0xff0d:
+      converted_key = VK_RETURN;
+      break;
+    case 0x12:
+      converted_key = VK_LWIN;
+      break;
+    case 0xff1b:
+      converted_key = VK_ESCAPE;
+      break;
+    case 0xff51:
+      converted_key = VK_LEFT;
+      break;
+    case 0xff52:
+      converted_key = VK_UP;
+      break;
+    case 0xff54:
+      converted_key = VK_DOWN;
+      break;
+    case 0xff53:
+      converted_key = VK_RIGHT;
+      break;
+    case 0xff57:
+      converted_key = VK_END;
+      break;
+    case 0xff56:
+      converted_key = VK_NEXT;
+      break;
+    case 0xff55:
+      converted_key = VK_PRIOR;
+      break;
+    case 0xff50:
+      converted_key = VK_HOME;
+      break;
+    case 0xffe3: //check if it is one of the modifier keys
+    case 0xffe9:
+        converted_key = 0x97;
+        break;
+    case 0xffeb:
+        converted_key = VK_LWIN;
+        break;
+
+    case ';':
+    case ':':
+        converted_key = VK_OEM_1;
+        break;
+    case '-':
+    case '_':
+        converted_key = VK_OEM_MINUS;
+        break;
+    case '+':
+    case '=':
+        converted_key = VK_OEM_PLUS;
+        break;
+    case '/':
+    case '?':
+        converted_key = VK_OEM_2;
+        break;
+    case '`':
+    case '~':
+        converted_key = VK_OEM_3;
+        break;
+    case '.':
+    case '>':
+        converted_key = VK_OEM_PERIOD;
+        break;
+    case ',':
+    case '<':
+        converted_key = VK_OEM_COMMA;
+        break;
+    case '[':
+    case '{':
+        converted_key = VK_OEM_4;
+        break;
+    case ']':
+    case '}':
+        converted_key = VK_OEM_6;
+        break;
+    case '\\':
+    case '|':
+        converted_key = VK_OEM_5;
+        break;
+    case '\'':
+    case '"':
+        converted_key = VK_OEM_7;
+        break;
+
+    default:
+      //if (ispunct(key))
+      //  converted_key = '\0';
+      if (isalpha(key))
+        converted_key = toupper(key);
+      else if (isdigit(key))
+        converted_key = key;
+      else
+        converted_key = 0x97;
+      break;
+  } //end switch
+
+  return converted_key;
+} //end char XConvertKey(const uint16_t key) {
+
+char XTranslateModifier(char modifier) {
+  char converted_key = 0;
+  switch(modifier) {
+    case 0x01:
+      converted_key = VK_SHIFT;
+      break;
+    case 0x02:
+      break;
+    case 0x04:
+      converted_key = VK_CONTROL;
+      break;
+    case 0x08:
+      converted_key = VK_MENU;
+      break;
+    case 0x10:
+      break;
+    case 0x20:
+      break;
+    case 0x40:
+      converted_key = VK_LWIN;
+      break;
+    case 0x80:
+      break;
+  } //end switch
+
+  return converted_key;
+} //end char XTranslateModifier(char modifier) {
